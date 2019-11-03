e7abb9d56d1c8e948ecf26a2d9b2543b1c9f534b
Jim Thoburn <jim@jimthoburn.com>
2019-11-03 14:10:00 -0800

    Add an index page

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,37 @@
 import { createElement }  from "../web_modules/preact.js";
 import   htm              from "../web_modules/htm.js";
 const    html = htm.bind(createElement);
-import { PictureGallery } from "../components/picture-gallery.js";
 
 
-function IndexPage({ album, pictures, getPageURL }) {
-
+function IndexPage({ title, date, albums }) {
+  
   return html`
-    <${PictureGallery}
-      album="${album}"
-      pictures="${pictures}"
-      getPageURL="${getPageURL}" />
+    <section class="picture-list">
+      <h1>${ title }</h1>
+      
+      ${ (date)
+         ? html`<p>${ date }</p>`
+         : "" }
+
+      <ol>
+        ${albums.map((album, index) => {
+          const cover = album.pictures[album.cover_picture_index];
+          const picture = cover ? cover : album.pictures[0];
+          return html`
+          <li>
+            <a href="/${album.uri}/">
+              <responsive-image>
+                <img src="${ picture.source }"
+                     width="320" />
+                <span class="caption">${ album.title }</span>
+              </responsive-image>
+            </a>
+          </li>
+          `
+        })}
+      </ol>
+    </section>
+
   `;
 }
 
