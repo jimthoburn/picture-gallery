6222f972685cd8c7a44b79bd8f68284114184007
Jim Thoburn <jim@jimthoburn.com>
2021-03-21 20:19:54 -0700

    Avoid passive listener warning in Chrome

--- helpers/move-gesture.js
+++ helpers/move-gesture.js
@@ -4,7 +4,6 @@ import { useEffect } from "../web_modules/preact/hooks.js";
 import { getTouchPosition, multiTouch } from "../helpers/touch.js";
 import { isBrowser } from "../helpers/environment.js";
 
-
 function useMoveGesture({ targetRef, onMoveStart, onMove, onRelease }) {
 
   function onTouchStart(e) {
@@ -29,13 +28,17 @@ function useMoveGesture({ targetRef, onMoveStart, onMove, onRelease }) {
   // Listen for touch events
   useEffect(() => {
     if (isBrowser() && targetRef.current) {
-      targetRef.current.addEventListener("touchstart", onTouchStart);
-      targetRef.current.addEventListener("touchmove", onTouchMove);
-      targetRef.current.addEventListener("touchend", onTouchEnd);
+
+      // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+      const options = { passive: false };
+
+      targetRef.current.addEventListener("touchstart", onTouchStart, options);
+      targetRef.current.addEventListener("touchmove", onTouchMove, options);
+      targetRef.current.addEventListener("touchend", onTouchEnd, options);
       return () => {
-        targetRef.current.removeEventListener("touchstart", onTouchStart);
-        targetRef.current.removeEventListener("touchmove", onTouchMove);
-        targetRef.current.removeEventListener("touchend", onTouchEnd);
+        targetRef.current.removeEventListener("touchstart", onTouchStart, options);
+        targetRef.current.removeEventListener("touchmove", onTouchMove, options);
+        targetRef.current.removeEventListener("touchend", onTouchEnd, options);
       };
     }
   }, [targetRef]);
