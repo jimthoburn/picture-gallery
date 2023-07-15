003a7e109c1d25564e885a636d3104e1e3fe7b5d
Jim Thoburn <jim@tobbi.co>
2023-07-15 12:39:11 -0700

    Bump `deno` from 1.34.3 to 1.35.0
    Use built-in `fetch` for Deno.

--- create/albums-deno.js
+++ create/albums-deno.js
@@ -1,6 +1,11 @@
 import { load } from "dotenv/mod.ts";
 import { createAlbums } from "./albums.js";
 
+const readFile = async (filePath) => {
+  const blob = await Deno.readFile(filePath);
+  return blob;
+};
+
 // https://deno.land/std/dotenv/mod.ts
 const env = await load();
-createAlbums({ env });
+createAlbums({ env, fetch, readFile });
--- create/albums-node.js
+++ create/albums-node.js
@@ -1,4 +1,14 @@
 import { createAlbums } from "./albums.js";
 
+import fetch, {
+  fileFromSync,
+} from "node-fetch";
+
+const readFile = async (filePath) => {
+  const mimetype = "image/jpeg";
+  const blob = fileFromSync(filePath, mimetype);
+  return blob;
+}
+
 const env = globalThis.process.env;
-createAlbums({ env });
+createAlbums({ env, fetch, readFile });
