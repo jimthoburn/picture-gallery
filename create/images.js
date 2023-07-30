5b605d951e396eba0bf81b4be7eee83722dee4b7
Jim Thoburn <jim@tobbi.co>
2023-07-29 18:31:00 -0700

    Create image formats & sizes with web workers

--- create/images.js
+++ create/images.js
@@ -2,11 +2,10 @@ import fs from "node:fs";
 import { mkdirp } from "mkdirp";
 import sizeOf from "image-size";
 import chalk from "chalk";
-import wasmImageProcessingFactory from "wasm-vips/lib/vips-es6.js";
 
 import { getAlbumNamesFromPicturesFolder } from "../data-file-system/albums-from-pictures-folder.js";
 
-const overwrite = false; // Set this “true” to re-generate existing image files.
+const overwrite = false; // Set this “true” to re-create existing image files.
 const startTime = Date.now();
 
 const configData  = JSON.parse(fs.readFileSync("./_api/config.json", "utf8"));
@@ -26,316 +25,106 @@ const SIZES = [
   6000,
 ];
 
-const wasmImageProcessingOptions = {
-  // print: (stdout) => console.log(stdout.replace(/\033\[[0-9;]*m/g, "")),
-  // Uncomment to disable dynamic modules
-  // dynamicLibraries: [],
-  // Test all available dynamic modules
-  // dynamicLibraries: ['vips-jxl.wasm', 'vips-heif.wasm', 'vips-resvg.wasm'],
-  // preRun: (module) => {
-    // module.setAutoDeleteLater(true);
-    // module.setDelayFunction(fn => {
-    //   globalThis.cleanup = fn;
-    // });
+async function createOneImage({ sourceFile, destinationFolder }) {
 
-    // Handy for debugging
-    // module.ENV.VIPS_INFO = '1';
-    // module.ENV.VIPS_LEAK = '1';
+  const pathBits = sourceFile.split("/");
+  const fileName = pathBits[pathBits.length - 1];
+  const fileNameBits = fileName.split(".");
+  const fileNameBase = fileNameBits.slice(0, fileNameBits.length - 1).join(".");
 
-    // Preload test files
-    // for (const file of Helpers.testFiles)
-    //   module.FS.createPreloadedFile('/', file.split('/').pop(), 'test/unit/images/' + file, true, false);
-  // },
-  postRun: (module) => {
-    const pad = 30;
-    const have = (name) => module.Utils.typeFind('VipsOperation', name) !== 0;
+  const sourceFileSize = sizeOf(sourceFile); // https://github.com/image-size/image-size
 
-    console.log('vips version:'.padEnd(pad), module.version());
-    console.log('Emscripten version:'.padEnd(pad), module.emscriptenVersion());
-    //console.log(module.config());
-    console.log('Concurrency:'.padEnd(pad), module.concurrency());
+  console.log("");
+  console.log(chalk.cyan("- - - - - - - - - - - - - - - - - - - - - - -"));
+  console.log(chalk.cyan(`createOneImage`));
+  console.log("");
+  console.log({ sourceFileSize, destinationFolder, fileNameBase });
+  console.log(chalk.cyan("- - - - - - - - - - - - - - - - - - - - - - -"));
+  console.log("");
 
-    console.log('Cache max mem:'.padEnd(pad), module.Cache.maxMem());
-    console.log('Cache max operations:'.padEnd(pad), module.Cache.max());
-    console.log('Cache current operations:'.padEnd(pad), module.Cache.size());
-    console.log('Cache max open files:'.padEnd(pad), module.Cache.maxFiles());
+  const imagesToCreate = {};
 
-    console.log('Memory allocations:'.padEnd(pad), module.Stats.allocations());
-    console.log('Memory currently allocated:'.padEnd(pad), module.Stats.mem());
-    console.log('Memory high water:'.padEnd(pad), module.Stats.memHighwater());
-    console.log('Open files:'.padEnd(pad), module.Stats.files());
+  for (const width of SIZES) {
+    imagesToCreate[width] = {};
 
-    console.log('JPEG support:'.padEnd(pad), have('jpegload') ? 'yes' : 'no');
-    console.log('JPEG XL support:'.padEnd(pad), have('jxlload') ? 'yes' : 'no');
-    console.log('AVIF support:'.padEnd(pad), have('heifload') ? 'yes' : 'no');
-    console.log('PNG support:'.padEnd(pad), have('pngload') ? 'yes' : 'no');
-    console.log('TIFF support:'.padEnd(pad), have('tiffload') ? 'yes' : 'no');
-    console.log('WebP support:'.padEnd(pad), have('webpload') ? 'yes' : 'no');
-    console.log('GIF support:'.padEnd(pad), have('gifload') ? 'yes' : 'no');
-
-    console.log('SVG load:'.padEnd(pad), have('svgload') ? 'yes' : 'no');
-
-    console.log('Text rendering support:'.padEnd(pad), have('text') ? 'yes' : 'no');
-  }
-};
-
-function generateOneImage({ width, sourceFile, destinationFolder }) {
-  return new Promise(async (resolve, reject) => {
-
-    const pathBits = sourceFile.split("/");
-    const fileName = pathBits[pathBits.length - 1];
-    const fileNameBits = fileName.split(".");
-    const fileNameBase = fileNameBits.slice(0, fileNameBits.length - 1).join(".");
-
-    const sourceFileSize = sizeOf(sourceFile); // https://github.com/image-size/image-size
-
-    console.log("");
-    console.log(chalk.cyan("- - - - - - - - - - - - - - - - - - - - - - -"));
-    console.log(chalk.cyan(`generateOneImage`));
-    console.log({ sourceFileSize, destinationFolder, fileNameBase });
-    console.log(chalk.cyan("- - - - - - - - - - - - - - - - - - - - - - -"));
-    console.log("");
-
-    const jpegFileName = `${destinationFolder}/${fileNameBase}.jpeg`;
-    const webpFileName = `${destinationFolder}/${fileNameBase}.webp`;
-    const avifFileName = `${destinationFolder}/${fileNameBase}.avif`;
-
-    const imageFormatsToCreate = {
-      jpeg: false,
-      webp: false,
-      avif: false,
-    };
+    const jpegFileName = `${destinationFolder}/${width}-wide/${fileNameBase}.jpeg`;
+    const webpFileName = `${destinationFolder}/${width}-wide/${fileNameBase}.webp`;
+    const avifFileName = `${destinationFolder}/${width}-wide/${fileNameBase}.avif`;
 
     if (!fs.existsSync(jpegFileName) || overwrite === true) {
-      imageFormatsToCreate.jpeg = true;
+      imagesToCreate[width].jpeg = jpegFileName;
     } else {
       console.log(``);
-      console.log(`${jpegFileName} already exists. Skipping…`);
+      console.log(`✅ `, `Found ${jpegFileName} file (keeping existing image)`);
       console.log(``);
     }
 
     if (width > 16 && configData.imageFormats && configData.imageFormats.webp && (!fs.existsSync(webpFileName) || overwrite === true)) {
-      imageFormatsToCreate.webp = true;
+      imagesToCreate[width].webp = webpFileName;
     } else {
       if (fs.existsSync(webpFileName)) {
         console.log(``);
-        console.log(`${webpFileName} already exists. Skipping…`)
+        console.log(`✅ `, `Found ${webpFileName} file (keeping existing image)`);
         console.log(``);
       };
     }
 
     if (width > 16 && configData.imageFormats && configData.imageFormats.avif && (!fs.existsSync(avifFileName) || overwrite === true)) {
-      imageFormatsToCreate.avif = true;
+      imagesToCreate[width].avif = avifFileName;
     } else {
       if (fs.existsSync(avifFileName)) {
         console.log(``);
-        console.log(`${avifFileName} already exists. Skipping…`);
+        console.log(`✅ `, `Found ${avifFileName} file (keeping existing image)`);
         console.log(``);
       }
     }
 
-    if (imageFormatsToCreate.jpeg || imageFormatsToCreate.webp || imageFormatsToCreate.avif) {
-
-      console.log("");
-      console.log(chalk.cyan("- - - - - - - - - - - - - - - - - - - - - - -"));
-      console.log("⏳ ", chalk.cyan("Starting WebAssembly..."));
-      console.log("");
-      const wasmImageProcessingInstance = await wasmImageProcessingFactory();
-      // const wasmImageProcessingInstance = await wasmImageProcessingFactory(wasmImageProcessingOptions);
-      console.log("");
-      console.log("🏁", chalk.cyan("WebAssembly ready"));
-      console.log(chalk.cyan("- - - - - - - - - - - - - - - - - - - - - - -"));
-      console.log("");
-
-      // Load an image from a file
-      const originalImageBlob = await Deno.readFile(sourceFile);
-      console.log({ originalImageBlob });
-
-      // Load an image from a preloaded file
-      const source = wasmImageProcessingInstance.Source.newFromMemory(originalImageBlob);
-      const originalImage = wasmImageProcessingInstance.Image.newFromSource(source);
-
-      /**
-       * resize(scale, options)
-       * @see https://github.com/kleisauke/wasm-vips/blob/a9a1d86b611f94bdca025b5521317371d65f2540/lib/vips.d.ts
-       * For example, if the original image is 2000px wide, and we want a 1000px wide image,
-       * we need to resize it to 0.5 (1000 / 2000) of its original size.
-       */
-      const scale = width / sourceFileSize.width;
-      const resizedImage =
-        (width < sourceFileSize.width)
-        ? await originalImage.resize(scale)
-        : originalImage;
-
-      const createImage = async ({ format, quality, optionsString, withMetaData, resizedImage, outputFile }) => {
-        const fileOptionsString = `[Q=${quality}${optionsString ? `,${optionsString}` : ''}${withMetaData === false ? ",strip=1" : ""}]`;
-        console.log({ options: fileOptionsString });
-
-        // Write the result to a buffer
-        const outputImageBlob = resizedImage.writeToBuffer(`.${format}${fileOptionsString}`);
-
-        // Write the buffer to a file
-        Deno.writeFile(outputFile, outputImageBlob);
-      }
-
-      const withMetaData = width >= 2048;
-
-      if (imageFormatsToCreate.jpeg) {
-        console.log(``);
-        console.log(`🖼  Generating JPEG`);
-        console.log(``);
-
-        /**
-         * @see https://github.com/kleisauke/wasm-vips/blob/a9a1d86b611f94bdca025b5521317371d65f2540/lib/vips.d.ts
-         */
-        const options = {
-          trellis_quant: 1, // jpegTrellisQuantisation = true;
-          overshoot_deringing: 1, // jpegOvershootDeringing = true;
-          optimize_scans: 1, // jpegOptimiseScans = true;
-          interlace: 1, // jpegProgressive = true;
-          quant_table: 3, // jpegQuantisationTable = 3;
-        }
-        // key=value,key=value,key=value
-        const optionsString = Object.entries(options).map(([ key, value ]) => `${key}=${value}`).join(",")
-
-        createImage({ resizedImage, outputFile: jpegFileName, format: 'jpeg', withMetaData, quality: 65, optionsString });
-      }
-
-      if (imageFormatsToCreate.webp) {
-        console.log(``);
-        console.log(`🖼  Generating WebP`);
-        console.log(``);
-
-        /**
-         * @see https://github.com/kleisauke/wasm-vips/blob/a9a1d86b611f94bdca025b5521317371d65f2540/lib/vips.d.ts
-         */
-        const options = {
-          effort: 4,
-        }
-        // key=value,key=value,key=value
-        const optionsString = Object.entries(options).map(([ key, value ]) => `${key}=${value}`).join(",")
-
-        createImage({ resizedImage, outputFile: webpFileName, format: 'webp', withMetaData, quality: 50, optionsString });
-      }
-
-      if (imageFormatsToCreate.avif) {
-        console.log(``);
-        console.log(`🖼  Generating AVIF`);
-        console.log(``);
-
-        /**
-         * @see https://github.com/kleisauke/wasm-vips/blob/a9a1d86b611f94bdca025b5521317371d65f2540/lib/vips.d.ts
-         */
-        const options = {
-          effort: 4,
-          compression: "av1", // encoder to use.
-          subsample_mode: "on", // heifChromaSubsampling == "4:4:4" ? "on" : "off",
-          bitdepth: 8, // Number of bits per pixel.
-        }
-        // key=value,key=value,key=value
-        const optionsString = Object.entries(options).map(([ key, value ]) => `${key}=${value}`).join(",")
-
-        createImage({ resizedImage, outputFile: avifFileName, format: 'avif', withMetaData, quality: 50, optionsString });
-      }
-
-      console.log("");
-      console.log(chalk.cyan("- - - - - - - - - - - - - - - - - - - - - - -"));
-      console.log("⏳ ", chalk.cyan("Shutting down WebAssembly..."));
-      wasmImageProcessingInstance.shutdown();
-      console.log("");
-      console.log("✅ ", chalk.cyan("WebAssembly shutdown finished"));
-      console.log(chalk.cyan("- - - - - - - - - - - - - - - - - - - - - - -"));
-      console.log("");
-    }
-
-    console.log("");
-    console.log(`⏱️ Time elapsed:\n${Date.now() - startTime} milliseconds\n`); 
-    console.log("");
-
-    resolve();
-  });
-}
-
-
-async function generateImages({ width, imagePath }) {
-
-  console.log("");
-  console.log(chalk.cyan("- - - - - - - - - - - - - - - - - - - - - - -"));
-  console.log("⏱️ ", chalk.cyan('generateImages: ' + width + ' :: ' + imagePath));
-  console.log(chalk.cyan("- - - - - - - - - - - - - - - - - - - - - - -"));
-  console.log("");
-  console.log(`Time elapsed:\n${Date.now() - startTime} milliseconds\n`);
-  console.log("");
-
-  const sourceFolder      = `${imagePath}/original`;
-  const destinationFolder = `${imagePath}/${width}-wide`;
-
-  const files = getAllFilesFromFolder(sourceFolder);
-
-  try {
-    await mkdirp(destinationFolder);
-
-    for (let index = 0; index < files.length; index++) {
-      const sourceFile = files[index];
-      const pathBits = sourceFile.split("/");
-      const fileName = pathBits[pathBits.length - 1];
-      console.log(chalk.cyan(`${nextFolderCursor} of ${albums.length} albums`));
-      console.log(chalk.cyan(`${nextCursor} of ${SIZES.length} sizes`));
-      console.log(chalk.cyan(`${index + 1} of ${files.length} pictures`));
-      await generateOneImage({
-        width,
-        sourceFile,
-        destinationFolder,
-      });
-    }
-
-  } catch(e) {
-    console.error(e);
   }
 
-  generateNextSize();
-}
+  const sizesWithImagesToCreate = SIZES.filter(width => imagesToCreate[width].jpeg || imagesToCreate[width].webp || imagesToCreate[width].avif);
 
-let nextCursor = 0;
-let nextImagePath;
-function generateNextSize() {
-  if (nextCursor < SIZES.length) {
-    console.log('generateNext: ' + nextCursor + ' :: ' + SIZES[nextCursor] + ' :: ' + nextImagePath);
-    generateImages({
-      width: SIZES[nextCursor],
-      imagePath: nextImagePath
-    });
-    nextCursor++;
-  } else {
-    generateNextFolder();
+  if (sizesWithImagesToCreate.length > 0) {
+
+    // Load an image from a file
+    const originalImageBlob = await Deno.readFile(sourceFile);
+    // console.log({ originalImageBlob });
+
+    // console.log({ imagesToCreate });
+
+    await Promise.all(sizesWithImagesToCreate.map(
+      (width) => {
+        return new Promise(async (resolve, reject) => {
+          const imageDescription = `${destinationFolder}/${width}-wide/${fileNameBase}`;
+
+          const worker = new Worker(
+            new URL("./images-worker.js", import.meta.url).href,
+            {
+              type: "module",
+            },
+          );
+          worker.onmessage = (e) => {
+            // console.log("");
+            // console.log(chalk.cyan("- - - - - - - - - - - - - - - - - - - - - - -"));
+            // console.log("✅ ", chalk.cyan(`Finished creating image at size ${width}px for ${sourceFile}`));
+            // console.log(chalk.cyan("- - - - - - - - - - - - - - - - - - - - - - -"));
+            // console.log("");
+
+            // console.log("📦", "Message received from worker");
+            // console.log({ data: e.data });
+
+            resolve();
+          };
+          worker.postMessage({ imagesToCreate, width, originalImageBlob, sourceFileSize, imageDescription });
+        });
+      }
+    ));
+
+    console.log("");
+    console.log(`⏱️ `, `Time elapsed:\n${Date.now() - startTime} milliseconds\n`); 
+    console.log("");
   }
 }
 
-let nextFolderCursor = 0;
-function generateNextFolder() {
-  if (nextFolderCursor < albums.length) {
-    console.log('generateNextFolder: ' + nextFolderCursor + ' :: ' + albums[nextFolderCursor]);
-
-    nextCursor = 0;
-    nextImagePath = `_pictures/${ albums[nextFolderCursor] }`;
-    generateNextSize();
-
-    nextFolderCursor++;
-  } else {
-
-    console.log("");
-    console.log(chalk.cyan("- - - - - - - - - - - - - - - - - - - - - - -"));
-    console.log("🏁", chalk.cyan(`Finished creating images`));
-    console.log(chalk.cyan("- - - - - - - - - - - - - - - - - - - - - - -"));
-    console.log("");
-    console.log(`Total time elapsed: ${Date.now() - startTime} milliseconds`);
-    console.log("");
-
-    // For WebAssembly
-    // https://esbuild.github.io/getting-started/#deno
-    Deno.exit(0);
-  }
-}
 
 // https://stackoverflow.com/questions/20822273/best-way-to-get-folder-and-file-list-in-javascript#21459809
 function getAllFilesFromFolder(dir) {
@@ -360,7 +149,65 @@ function getAllFilesFromFolder(dir) {
   return results
 }
 
-// Generate images
-nextFolderCursor = 0;
-generateNextFolder();
 
+// Create a folder for each image size
+async function createAlbumFolders({ albumPath }) {
+  for (const width of SIZES) {
+    try {
+      await mkdirp(`${albumPath}/${width}-wide`);
+    } catch(e) {
+      console.error(e);
+    }
+  }
+}
+
+async function createAlbumImages({ albumIndex, albumPath, originalImageFiles }) {
+  
+  // Create sizes and formats for each of the images
+  await Promise.all(originalImageFiles.map(
+    async (sourceFile, imageIndex) => {
+      console.log("");
+      console.log(chalk.cyan("- - - - - - - - - - - - - - - - - - - - - - -"));
+      console.log("⏱️ ", chalk.cyan('Creating images for album: ' + albumPath));
+      console.log(chalk.cyan("- - - - - - - - - - - - - - - - - - - - - - -"));
+      console.log("");
+      console.log(`Time elapsed:\n${Date.now() - startTime} milliseconds\n`);
+      console.log("");
+    
+      console.log(chalk.cyan(`${albumIndex + 1} of ${albums.length} albums`));
+      console.log(chalk.cyan(`${imageIndex + 1} of ${originalImageFiles.length} pictures`));
+
+      await createOneImage({
+        sourceFile,
+        destinationFolder: albumPath,
+      });
+    }
+  ));
+}
+
+async function createImages() {
+  for (let albumIndex = 0; albumIndex < albums.length; albumIndex++) {
+    const album = albums[albumIndex];
+    console.log(`creating next album: ${album}`);
+
+    const albumPath = `_pictures/${ album }`;
+    const originalImageFiles = getAllFilesFromFolder(`${albumPath}/original`);
+
+    await createAlbumFolders({ albumPath });
+    await createAlbumImages({ albumIndex, albumPath, originalImageFiles });
+  }
+
+  console.log("");
+  console.log(chalk.cyan("- - - - - - - - - - - - - - - - - - - - - - -"));
+  console.log("🏁", chalk.cyan(`Finished creating images`));
+  console.log(chalk.cyan("- - - - - - - - - - - - - - - - - - - - - - -"));
+  console.log("");
+  console.log(`Total time elapsed: ${Date.now() - startTime} milliseconds`);
+  console.log("");
+
+  // For WebAssembly
+  // https://esbuild.github.io/getting-started/#deno
+  Deno.exit(0);
+}
+
+createImages();
